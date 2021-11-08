bddea9e1ae5dfa0d8c4cbe7d5a39b317a26591d9
kangilbin <kib3388@naver.com>
2021-11-08 22:14:09 +0900

    "라우팅"

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Header />
+    <div id="content" class="content"></div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style>
+</style>
